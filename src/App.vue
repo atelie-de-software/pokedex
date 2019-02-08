06dd387f4f7a8f4e9783b55ce8799c487aad3761
WebGoal <webgoal@iMac-de-Igor-Marketing.local>
2019-02-08 17:27:40 -0200

    Scaffold

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">Home</router-link>
     </div>
-    <router-view/>
+    <main>
+      <router-view/>
+    </main>
+    <footer>
+      Curso de vue - 2019
+    </footer>
   </div>
 </template>
 
@@ -27,3 +31,9 @@
   }
 }
 </style>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
